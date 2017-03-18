a114269d93247901a9ea4d6810d82e1b90589a10
Stephen <stephen@whitebolt.net>
2017-03-18 13:53:10 +0000

    Add footer

--- photographyinyork/footer.php
+++ photographyinyork/footer.php
@@ -1,4 +1,5 @@
-<footer>
+<footer class="row medium-12 columns">
+	Copyright &copy; and design 2017 by <a href="http://whitebolt.net/templates/photographyinyork">Whitebolt LTD</a>
 	<?php wp_footer(); ?>
 </footer>
 </body>
