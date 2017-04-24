6e91cbd6431addc2f83bcd0ba2c750c7599ec19a
Stephen <stephen@whitebolt.net>
2017-04-24 12:34:21 +0100

    Remove other themes
    This is not achild theme any more so remove connection to parent.

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?php
-// Silence is golden.
+<?php get_header(); ?>
+	<main>
+		<?php $header_block = types_render_field( 'header-block', array('output' => 'raw')); ?>
+		<?php $hide_title = types_render_field( 'hide-title', array()); ?>
+		<?php echo $header_block; ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article<?php echo (($header_block == '') ? ' shift-content' : ''); ?> class="row medium-12 columns">
+				<?php if ($hide_title != "1") { ?>
+					<h1><?php the_title(); ?></h1>
+				<?php } ?>
+				<?php the_content(); ?>
+			</article>
+		<?php endwhile; ?>
+	</main>
+<?php get_footer();
