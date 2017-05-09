bf9748f0b9cd9e10982f355ad84c499ec8ca904d
Stephen <stephen@whitebolt.net>
2017-05-09 11:47:22 +0100

    Fixing of footer on mobile

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <footer class="row medium-12 columns">
 	<div class="footer-main row">
-		<div class="medium-4 columns footer-main-block1">
+		<div class="medium-3 columns footer-main-block1">
 			<a href="<?php echo get_home_url(); ?>" class="logo-wrap"><img src="<?php echo PIY_DIR.'/images/logo.png'; ?>" class="logo" /></a>
 		</div>
-		<div class="medium-4 columns footer-main-block2"><i class="fa fa-phone" aria-hidden="true"></i> Make a booking 01234 56789</div>
-		<div class="medium-4 columns footer-main-block3">
+		<div class="medium-6 columns footer-main-block2"><i class="fa fa-phone" aria-hidden="true"></i> Make a booking 01234 56789</div>
+		<div class="medium-3 columns footer-main-block3">
 			<?php echo wp_nav_menu( array(
 				'theme_location' => 'social',
 				'container' => false,
